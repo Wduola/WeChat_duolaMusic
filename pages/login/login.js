55800a6571c788ef542beabf33f68e07bdd4eec9
wangbaohong <q1329462887@gmail.com>
2020-07-07 23:44:04 +0800

    哆啦音乐：个人中心完成，登录页面跳转完成

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,66 +1,100 @@
 // pages/login/login.js
-Page({
+import request from "../../utils/request";
 
+Page({
   /**
    * 页面的初始数据
    */
   data: {
-
+    phone: "",
+    password: "",
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function (options) {},
 
+  // 收集表单数据
+  handleInput(e) {
+    let type = e.currentTarget.dataset.type;
+    this.setData({
+      [type]: e.detail.value,
+    });
+  },
+  // 登陆流程
+  async login() {
+    // 一、收集表单数据
+    let { phone, password } = this.data;
+    // 二、前端验证
+    if (!phone || !password) {
+      // 前端验证：用户名和密码都错，提示
+      this.showToast("用户名/密码错误");
+    } else {
+      // 前端验证：用户名和密码均正确
+      // 三、后端验证
+      let result = await request(`/login/cellphone?phone=${phone}&password=${password}`);
+      console.log("表单验证数据：", result);
+      // 提示信息
+      if (result.code === 501) {
+        // 手机号错误
+        this.showToast("手机号错误");
+      } else if (result.code === 502) {
+        // 密码错误
+        this.showToast("密码错误");
+      } else {
+        // 登陆成功
+        this.showToast("登陆成功", "seccess");
+
+        // 1.保存用户数据至本地setStorageSync
+        wx.setStorageSync("userInfo", JSON.stringify(result.profile));
+        // 2.跳转至个人中心
+        wx.switchTab({
+          url: "/pages/personal/personal",
+        });
+      }
+    }
+  },
+  // 用于提示消息的功能函数
+  showToast(title = "", icon = "none") {
+    wx.showToast({
+      title,
+      icon,
+    });
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
-  },
+  onReady: function () {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
-  },
+  onShow: function () {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
-  },
+  onHide: function () {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-
-  },
+  onUnload: function () {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function () {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
-  },
+  onReachBottom: function () {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
-  }
-})
+  onShareAppMessage: function () {},
+});
--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -1,66 +1,108 @@
 // pages/personal/personal.js
-Page({
+import request from "../../utils/request";
+let startY = 0; //触摸开始位置
+let moveY = 0; //触摸移动起始位置
+let distance = 0; //距离差
 
+Page({
   /**
    * 页面的初始数据
    */
   data: {
-
+    coverTransform: "", //位移
+    coverTransition: "", //过度
+    userInfo: {}, //用户信息
+    recentPlayList: [], //用户播放记录
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: async function (options) {
+    // 判断本地是否有用户登录信息
+    let userInfo = wx.getStorageSync("userInfo");
+    console.log("用户信息", userInfo);
+    if (userInfo) {
+      this.setData({
+        userInfo: JSON.parse(userInfo),
+      });
+      // 获取用户播放信息
+      let recentPlayListData = await request(`/user/record?uid=${this.data.userInfo.userId}&type=0`);
+      this.setData({
+        recentPlayList: recentPlayListData.allData,
+      });
+    }
+  },
 
+  // 触摸开始
+  handleTouchStart(e) {
+    // 触摸向下拉没有过度效果
+    this.setData({
+      coverTransition: "",
+    });
+    // 获取触摸开始位置
+    startY = e.touches[0].clientY;
+  },
+  // 触摸移动
+  handleTouchMove(e) {
+    // 手指移动结束位置
+    moveY = e.touches[0].clientY;
+    // 距离差
+    distance = moveY - startY;
+    // 判断极端位置
+    if (distance <= 0) {
+      return;
+    }
+    if (distance >= 80) {
+      distance = 80;
+    }
+    // 更新coverTransfrom的状态
+    this.setData({
+      coverTransform: `translateY(${distance}px)`,
+    });
+  },
+  // 触摸结束
+  handleTouchEnd() {
+    this.setData({
+      // 位移
+      coverTransform: `translateY(0px)`,
+      // 过度
+      coverTransition: "transform .5s linear",
+    });
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
-  },
+  onReady: function () {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
-  },
+  onShow: function () {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
-  },
+  onHide: function () {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-
-  },
+  onUnload: function () {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function () {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
-  },
+  onReachBottom: function () {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
-  }
-})
+  onShareAppMessage: function () {},
+});
